fc6193ae67412fcebfa0f8169f9d442cb930374c
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-10-27 23:15:51 +0700

    main area

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,42 @@ function App() {
 
   return (
     <>
-      <h1>MultiChat App</h1>
+      <div class="container mx-auto">
+      <div class="min-w-full border rounded ">
+        <div class="hidden  lg:block">
+          <div class="w-full">
+            <div class="relative flex items-center p-3 border-b border-gray-300">
+              <span class="block ml-2 font-bold text-gray-600">Chatroom : General</span>
+            </div>
+            <div class="relative w-full p-6 overflow-y-auto h-[40rem]">
+              <ul class="space-y-2">
+                <li class="flex justify-start">
+                  <div class="relative max-w-xl px-4 py-2 text-gray-700 rounded shadow">
+                    <span class="block"> <strong>Jambrong</strong> Hi Bro!</span>
+                  </div>
+                </li>
+                <li class="flex justify-start">
+                  <div class="relative max-w-xl px-4 py-2 text-gray-700 rounded shadow">
+                    <span class="block"> <strong>Tukimin</strong> Hi juga Bro!</span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+
+            <div class="flex items-center justify-between w-full p-3 border-t border-gray-300">
+              
+
+              <input type="text" placeholder="Message"
+                class="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700"
+                name="message" required />
+              <button type="submit">
+                Send
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
